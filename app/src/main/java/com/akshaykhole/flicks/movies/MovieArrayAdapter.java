6dffc263e922dfe6d4bb5d2833469ee9b324de5f
Akshay Khole <akshay@reversehack.in>
2016-10-15 10:17:37 -0700

    tmp

--- app/src/main/java/com/akshaykhole/flicks/movies/MovieArrayAdapter.java
+++ app/src/main/java/com/akshaykhole/flicks/movies/MovieArrayAdapter.java
@@ -1,7 +1,15 @@
 package com.akshaykhole.flicks.movies;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.akshaykhole.flicks.R;
 
 import java.util.List;
 
@@ -12,5 +20,26 @@ import java.util.List;
 public class MovieArrayAdapter extends ArrayAdapter<MovieModel> {
     public MovieArrayAdapter(Context context, List<MovieModel> movies) {
         super(context, android.R.layout.simple_list_item_1, movies);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        MovieModel movie = getItem(position);
+
+        if(convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.movies_list_item, parent, false);
+        }
+
+        ImageView ivImage = (ImageView) convertView.findViewById(R.id.imageViewMoviePoster);
+        ivImage.setImageResource(0);
+
+        TextView tvTitle = (TextView) convertView.findViewById(R.id.textViewMovieTitle);
+        TextView tvOverView = (TextView) convertView.findViewById(R.id.textViewMovieOverview);
+
+        tvTitle.setText(movie.getOriginalTitle());
+        tvOverView.setText(movie.getOverview());
+        return convertView;
     }
 }
