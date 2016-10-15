ace57f30ededbf6ff4b6256b62f3b4be1e6cbffd
Akshay Khole <akshay@reversehack.in>
2016-10-15 16:53:45 -0700

    tmp

--- app/src/main/java/com/akshaykhole/flicks/movies/MovieDetailActivity.java
+++ app/src/main/java/com/akshaykhole/flicks/movies/MovieDetailActivity.java
@@ -1,15 +1,26 @@
 package com.akshaykhole.flicks.movies;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.akshaykhole.flicks.R;
 
 public class MovieDetailActivity extends AppCompatActivity {
+    TextView tvMovieTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
+        initComponents();
+        Intent intent = getIntent();
+        MovieModel movie = (MovieModel) intent.getSerializableExtra("movie");
+        tvMovieTitle.setText(movie.getOriginalTitle());
+    }
+
+    public void initComponents() {
+        this.tvMovieTitle = (TextView) findViewById(R.id.textViewMovieTitle);
     }
 }
